a75fbc6b3f277fd44b8cf21f7673463f6093e86c
MRIDUL SHARMA <108065487+mridulsharma000@users.noreply.github.com>
2022-07-15 13:47:24 +0530

    Create response.js

---
+++ response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    "what is your name": "I'm Chhotu, your personal assistant",
+    "can you chat with me in hindi?": "नमस्कार महोदय,मैं आपकी किस प्रकार सहायता कर सकता हूँ",
+    "who created you": "Mridul Sharma",
+  };
